4d66ca9e92c38ee8b6939fc274cd3c16e5a160ba
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-02-05 13:11:17 +0200

    Fixing init firebase

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyBR4XlSKoIKhzhKacW7n7APUbtCR4vHtx4",
     authDomain: "afrimapinternational.firebaseapp.com",
@@ -9,8 +7,51 @@
     appId: "1:486636940166:web:ce71e65f1410de61a11a07",
     measurementId: "G-S5LGGJMR2R"
   };
-  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+//   firebase.analytics();
+//   firebase.auth.Auth.Persistence.LOCAL;
 
-  
+  alert("init")
+
+  $("#btn-login").click(function(){
+
+    var email = $("#email").val();
+    var password = $("#password").val();
+
+    alert("clicking")
+
+    console.log("clicking");
+
+
+if(email != "" && password != ""){
+
+
+    var result = firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+
+    window.alert("Form is incomplete, please fill out all fields"); 
+
+
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(errorCode); 
+      console.log(errorMessage); 
+
+
+    });
+
+
+}else{
+    window.alert("Form is incomplete, please fill out all fields"); 
+
+
+}
+
+
+  })
+
