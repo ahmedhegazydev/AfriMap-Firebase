51534ea32d10233a72b6679e1b7aab828a957d36
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-02-05 11:18:58 +0200

    register

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBR4XlSKoIKhzhKacW7n7APUbtCR4vHtx4",
+    authDomain: "afrimapinternational.firebaseapp.com",
+    projectId: "afrimapinternational",
+    storageBucket: "afrimapinternational.appspot.com",
+    messagingSenderId: "486636940166",
+    appId: "1:486636940166:web:ce71e65f1410de61a11a07",
+    measurementId: "G-S5LGGJMR2R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  
